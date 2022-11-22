be431cc0d5631ae32b1533316e7fcbc8a02acd2f
Adrian <adrian.talonia11@gmail.com>
2022-11-21 23:42:32 -0600

    add new java version

--- src/main/java/com/javaapispringbootmaven/repository/UserRepository.java
+++ src/main/java/com/javaapispringbootmaven/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.javaapispringbootmaven.repository;
 
-public interface UserRepository {
+import com.javaapispringbootmaven.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
