404a5dd9f5228dc0c168b5563a83c4ffe8b21254
Adrian <adrian.talonia11@gmail.com>
2022-12-20 23:12:55 -0600

    add some changes

--- src/main/java/com/javaapispringbootmaven/repository/UserRepository.java
+++ src/main/java/com/javaapispringbootmaven/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.javaapispringbootmaven.repository;
 import com.javaapispringbootmaven.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
 }
